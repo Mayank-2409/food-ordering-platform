59e7bd7b42a7eec1673f45b89433280d87d6a6ba
Mayank240902 <mayankchoudhary2409@gmail.com>
2026-02-02 22:52:06 +0530

    chore: finalize stable frontend setup

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,7 +1,17 @@
+// const config = {
+//   plugins: {
+//     "@tailwindcss/postcss": {},
+//   },
+// };
+
+// export default config;
+
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
 
 export default config;
